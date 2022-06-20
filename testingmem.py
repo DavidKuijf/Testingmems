db846e45d14e7cbc7338db353c4c942a2733bb80
DavidKuijf <32140508+DavidKuijf@users.noreply.github.com>
2022-06-20 21:42:47 +0200

    333
    3333333

--- testingmem.py
+++ testingmem.py
@@ -7,9 +7,12 @@ import pyaudio
 import numpy as np
 
 CHUNK = 44100
+FORMAT = pyaudio.paInt16
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
 
@@ -21,9 +24,17 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 frames_per_buffer=CHUNK)
 
 
-for i in range(0, 10):
+# for i in range(0, 10):
+#     data = stream.read(CHUNK)
+#     print(np.frombuffer(data, np.float32))
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    print(np.frombuffer(data, np.float32))
+    frames.append(data)
+
+print("* done recording")
 
 
 
@@ -31,3 +42,10 @@ stream.stop_stream()
 stream.close()
 
 p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
