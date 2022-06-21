0974ae1612d10f2cc44bbeb66788ca11fc4c54f6
DavidKuijf <32140508+DavidKuijf@users.noreply.github.com>
2022-06-21 18:47:24 +0200

    ww
    wwww

--- testingmem.py
+++ testingmem.py
@@ -26,13 +26,22 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 frames_per_buffer=CHUNK)
 
 frames = deque([0,0,0,0,0,0,0,0,0,0])
+    
+def fft_calc(data_vec):
+    data_vec = data_vec*np.hanning(len(data_vec)) # hanning window
+    N_fft = len(data_vec) # length of fft
+    freq_vec = (float(CHUNK)*np.arange(0,int(N_fft/2)))/N_fft # fft frequency vector
+    fft_data_raw = np.abs(np.fft.fft(data_vec)) # calculate FFT
+    fft_data = fft_data_raw[0:int(N_fft/2)]/float(N_fft) # FFT amplitude scaling
+    fft_data[1:] = 2.0*fft_data[1:] # single-sided FFT amplitude doubling
+    return freq_vec,fft_data
 
 for i in range(0, 10):
     data = stream.read(CHUNK)
     frames.pop()
     frames.appendleft(data)
     print(np.frombuffer(data, np.int16))
-
+    print(fft_calc(data))
 
 stream.stop_stream()
 stream.close()
